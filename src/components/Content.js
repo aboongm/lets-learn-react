85aeb1b0de1c1316b74f01537435bf2771263ee2
aboongm <aboongm@yahoo.com>
2022-10-14 19:27:57 +0530

    Add functionalities for checkbox, doubleclick on text and delete

--- src/components/Content.js
+++ src/components/Content.js
@@ -24,14 +24,37 @@ const Content = () => {
       text: 'Learn Ruby On Rails',
     },
   ]);
+
+  const handleCheck = (id) => {
+    const newLists = lists.map((item) =>
+      item.id === id ? { ...item, checked: !item.checked } : item
+    );
+    setLists(newLists);
+  };
+
+  const handleDelete = (id) => {
+    const newLists = lists.filter((item) => item.id !== id);
+    setLists(newLists);
+  };
+
   return (
     <main>
       <ul className="list__items">
         {lists.map((item) => (
           <li key={item.id} className="item">
-            <input type="checkbox" checked={item.checked} />
-            <label htmlFor="text">{item.text}</label>
-            <FaTrashAlt />
+            <input
+              onChange={() => handleCheck(item.id)}
+              type="checkbox"
+              checked={item.checked}
+            />
+            <label
+              onDoubleClick={() => handleCheck(item.id)}
+              htmlFor="text"
+              style={item.checked ? { textDecoration: 'line-through' } : null}
+            >
+              {item.text}
+            </label>
+            <FaTrashAlt onClick={() => handleDelete(item.id)} />
           </li>
         ))}
       </ul>
